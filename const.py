e568834735ff558513d83293845a9e5a72716145
Vivek Choksi <viv.choksi@gmail.com>
2014-12-01 21:30:47 -0800

    Created data directory, and added pickups-aggregated.csv.

--- const.py
+++ const.py
@@ -2,7 +2,7 @@ class Const(object):
     
     DATABASE_NAME = 'taxi_pickups'
     AGGREGATED_PICKUPS = 'pickups_aggregated'
-    WEATHER_DATA = 'nyc_observed_weather.csv'
+    WEATHER_DATA = 'data/nyc_observed_weather.csv'
     TRIP_DATA = 'trip_data'
     TRAIN_BATCH_SIZE = 40000
 
