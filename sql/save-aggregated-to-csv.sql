c8aca14015150b8b5f9780c0a9ca8fb97525d4aa
Vivek Choksi <viv.choksi@gmail.com>
2014-12-10 14:18:36 -0800

    Changed sql script table names and zone boundaries so that we can include more months of data.

--- sql/save-aggregated-to-csv.sql
+++ sql/save-aggregated-to-csv.sql
@@ -3,11 +3,11 @@
 -- This script saves the pickups_aggregated table to a .csv file named
 -- pickups-aggregated.csv that is stored in the root mysql data
 -- directory. 
--- (For my Mac, this is /usr/local/mysql/data/taxi_pickups/pickups-aggregated.csv)
+-- (For my Mac, this is /usr/local/mysql/data/taxi_pickups/pickups-aggregated_manhattan.csv)
 
 SELECT *
-FROM pickups_aggregated
-INTO OUTFILE 'pickups-aggregated.csv'
+FROM pickups_aggregated_manhattan
+INTO OUTFILE 'pickups-aggregated_manhattan.csv'
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
